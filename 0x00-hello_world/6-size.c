ab0546d2d898113a07f7f811bbcb85800fd06073
YnsHm12 <ynshm1998@gmail.com>
2023-02-16 16:06:23 -0800

    alx-low_level_programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * imain - A program that prints the size of various computer types
+ * main - A program that prints the size of various computer types
  * Return: 0 (success)
  */
 int main(void)
