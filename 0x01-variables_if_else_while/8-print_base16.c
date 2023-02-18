e5f257ad8301397b67529083f3ae7a565611f431
YnsHm12 <ynshm1998@gmail.com>
2023-02-18 10:13:37 -0800

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: Print base 16 numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char i;
+	char j = '0';
+
+	while (j <= '9')
+	{
+		putchar(d);
+		j++;
+	}
+	for (i = 'a'; i <= 'f'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
